9ac0f5e0526c05d819b3b1b2a83e08932269827d
affsatheesh <65486093+affsatheesh@users.noreply.github.com>
2020-05-21 09:51:21 -0700

    now its running

--- Jankin_Test.java
+++ Jankin_Test.java
@@ -1,9 +1,7 @@
-public class Jankin_Test
+pulic Jenkin_Test
 {
-public static void main(String[] arg)
+ pulic static void main(String [] arg)
 {
-System.out.println("it is first jankin progarm");
-System.out.println("OMG");
-System.out.println("vaaruma varatha");
+System.out.println("its my first project");
 }
 }
